45c3b48d12ad3d9b36a77db2083ecc76fb171220
Your Name <masoudaminiw789@gmail.com>
2024-11-10 20:44:40 +0400

    change

--- actions/actions.js
+++ actions/actions.js
@@ -3,16 +3,33 @@ import { revalidatePath } from "next/cache";
 import prisma from "../lib/db";
 
 export async function createData(formData) {
-  if (!formData) {
-    throw new Error("formData is undefined");
+  try{
+    if (!formData) {
+      throw new Error("formData is undefined");
+    }
+    const title = formData.get("title");
+    const content = formData.get("content");
+    const slug = title.replace(/\s+/g, "-").toLowerCase(); 
+    // Check if a post with the same slug already exists
+    const existingPost = await prisma.post.findUnique({
+      where: { slug },
+    });
+    if (existingPost) {
+      throw new Error("A post with this title (slug) already exists.");
+    }
+    // Create a new post if slug is unique
+    await prisma.post.create({
+      data: {
+        title,
+        content,
+        slug,
+      },
+    });
+    // Revalidate cache for the posts page
+    revalidatePath("/posts");
   }
-
-  await prisma.post.create({
-    data: {
-      title: formData.get("title"),
-      content: formData.get("content"),
-      slug: formData.get("title").replace(/\s+/g, "-").toLowerCase(),
-    },
-  });
-  revalidatePath("/posts");
+  catch(error){
+    console.error(error);
+    throw error;
+  }
 }
